34156ee8f9cf4cd87f19ec8c939a87ea84547701
quiccklabs05 <adibiswas053@gmail.com>
2024-08-20 10:10:26 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,3 +1,20 @@
+public class Program6 {
+    public static void main(String [] args){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter the radius:");
+        double rad = 3.712;
+        System.out.println("The Radius of the circle is = " +rad);
+        double diameter = 7.424;
+        System.out.println("The Diameter of the circle is = " +diameter);
+        
+    }
+    
+}
+//Paste console output below:
+/*
+
+
+*/
 //import stuff here!
 
 //Your code here
