183c71ef7ba3e9818f6597bc4f207de261d374bd
quiccklabs05 <adibiswas053@gmail.com>
2024-08-23 09:33:36 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,26 +1,44 @@
+import java.util.Scanner;
+
+//import stuff here!
+
+//Your code here
 public class Program6 {
     public static void main(String [] args){
         Scanner myScanner = new Scanner(System.in);
+        
         System.out.println("Enter the radius:");
+        
         double rad = 3.712;
         System.out.println("The Radius of the circle is = " +rad);
-        double diameter = 7.424;
+        
+        double diameter = rad * 2;
         System.out.println("The Diameter of the circle is = " +diameter);
+        
+        double area = rad * rad * 3.14159;
+        double realarea = area;
+        double area1 = (realarea) * 1000;
+        area1 = (int) (area1+0.5);
+        area1 = (double) (area1/1000);
+        System.out.println("The Area of the circle is = " +area1);
+        
+        double circumference = rad * 2.0 * 3.14159;
+        double realcircumference = circumference;
+        double circumference1 = (realcircumference) * 1000;
+        circumference1 = (int) (circumference1+0.5);
+        circumference1 = (double) (circumference1/1000);
+        System.out.println("The Circumference of the circle is = " +circumference1);
         
     }
     
 }
 //Paste console output below:
+
 /*
-
-
-*/
-//import stuff here!
-
-//Your code here
-
-//Paste console output below:
-/*
-
+Enter the radius:
+The Radius of the circle is = 3.712
+The Diameter of the circle is = 7.424
+The Area of the circle is = 43.288
+The Circumference of the circle is = 23.323
 
 */
